5a4b376bcd2a4cb2b31772b1bb183f75fdd1c344
Ben Babcock <ben@tachyondecay.net>
2014-08-14 21:52:09 -0400

    Update jQuery UI, which makes autocomplete widget styling work with Symphony 2.4.

--- extension.driver.php
+++ extension.driver.php
@@ -21,7 +21,9 @@
 
 				// Let the jQuery magic flow 
 				if($context['oPage'] instanceof contentPublish) {
-					Administration::instance()->Page->addStylesheetToHead(URL . '/extensions/autocomplete_tags/assets/autocomplete_tags.jquery-ui-custom.css', 'screen', 100, false);
+					Administration::instance()->Page->addStylesheetToHead(URL . '/extensions/autocomplete_tags/assets/autocomplete_tags.jquery-ui.css', 'screen', 100, false);
+					Administration::instance()->Page->addStylesheetToHead(URL . '/extensions/autocomplete_tags/assets/autocomplete_tags.jquery-ui.structure.css', 'screen', 100, false);
+					Administration::instance()->Page->addStylesheetToHead(URL . '/extensions/autocomplete_tags/assets/autocomplete_tags.jquery-ui.theme.css', 'screen', 100, false);
 					Administration::instance()->Page->addScriptToHead(URL . '/extensions/autocomplete_tags/assets/autocomplete_tags.jquery-ui-custom.min.js', 100, false);
 					Administration::instance()->Page->addScriptToHead(URL . '/extensions/autocomplete_tags/assets/autocomplete_tags.publish.js', 200, false);
 				}
